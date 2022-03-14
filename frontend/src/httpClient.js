4eeb532cfcf48ca60f5d719b9c191e310e745418
Othmane Hanyf <othmane.hanyf@protonmail.com>
2022-03-14 23:26:32 +0100

    enhancing form and adding more fields, and adding favicon and logo

--- frontend/src/httpClient.js
+++ frontend/src/httpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8000/api/', //'https://photosrepo.herokuapp.com/api/',
+    baseURL: 'https://photosrepo.herokuapp.com/api/',
 })
 
 export default httpClient;
